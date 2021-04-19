6e8c02056c5c41f155d1ac25aebb979af575e445
Tu Nguyen <im2nguyen@gmail.com>
2021-04-19 14:43:11 -0700

    Add docs, goreleaser, and gh actions

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/hashicorp-demoapp/hashicups-client-go v0.0.0-20200508203820-4c67e90efb8e
+	github.com/hashicorp/terraform-plugin-docs v0.4.0 // indirect
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0-rc.2
 )
--- hashicups/resource_order.go
+++ hashicups/resource_order.go
@@ -69,84 +69,62 @@ func resourceOrder() *schema.Resource {
 			},
 		},
 		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
+			StateContext: schema.ImportStatePassthroughContext,
 		},
 	}
 }
 
 func resourceOrderCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*hc.Client)
-
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
-
 	items := d.Get("items").([]interface{})
 	ois := []hc.OrderItem{}
-
 	for _, item := range items {
 		i := item.(map[string]interface{})
-
 		co := i["coffee"].([]interface{})[0]
 		coffee := co.(map[string]interface{})
-
 		oi := hc.OrderItem{
 			Coffee: hc.Coffee{
 				ID: coffee["id"].(int),
 			},
 			Quantity: i["quantity"].(int),
 		}
-
 		ois = append(ois, oi)
 	}
-
 	o, err := c.CreateOrder(ois)
 	if err != nil {
 		return diag.FromErr(err)
 	}
-
 	d.SetId(strconv.Itoa(o.ID))
-
 	resourceOrderRead(ctx, d, m)
-
 	return diags
 }
-
 func resourceOrderRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*hc.Client)
-
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
-
 	orderID := d.Id()
-
 	order, err := c.GetOrder(orderID)
 	if err != nil {
 		return diag.FromErr(err)
 	}
-
 	orderItems := flattenOrderItems(&order.Items)
 	if err := d.Set("items", orderItems); err != nil {
 		return diag.FromErr(err)
 	}
-
 	return diags
 }
-
 func resourceOrderUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*hc.Client)
-
 	orderID := d.Id()
-
 	if d.HasChange("items") {
 		items := d.Get("items").([]interface{})
 		ois := []hc.OrderItem{}
-
 		for _, item := range items {
 			i := item.(map[string]interface{})
-
 			co := i["coffee"].([]interface{})[0]
 			coffee := co.(map[string]interface{})
-
 			oi := hc.OrderItem{
 				Coffee: hc.Coffee{
 					ID: coffee["id"].(int),
@@ -155,56 +133,41 @@ func resourceOrderUpdate(ctx context.Context, d *schema.ResourceData, m interfac
 			}
 			ois = append(ois, oi)
 		}
-
 		_, err := c.UpdateOrder(orderID, ois)
 		if err != nil {
 			return diag.FromErr(err)
 		}
-
 		d.Set("last_updated", time.Now().Format(time.RFC850))
 	}
-
 	return resourceOrderRead(ctx, d, m)
 }
-
 func resourceOrderDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*hc.Client)
-
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
-
 	orderID := d.Id()
-
 	err := c.DeleteOrder(orderID)
 	if err != nil {
 		return diag.FromErr(err)
 	}
-
 	// d.SetId("") is automatically called assuming delete returns no errors, but
 	// it is added here for explicitness.
 	d.SetId("")
-
 	return diags
 }
-
 func flattenOrderItems(orderItems *[]hc.OrderItem) []interface{} {
 	if orderItems != nil {
 		ois := make([]interface{}, len(*orderItems), len(*orderItems))
-
 		for i, orderItem := range *orderItems {
 			oi := make(map[string]interface{})
-
 			oi["coffee"] = flattenCoffee(orderItem.Coffee)
 			oi["quantity"] = orderItem.Quantity
 			ois[i] = oi
 		}
-
 		return ois
 	}
-
 	return make([]interface{}, 0)
 }
-
 func flattenCoffee(coffee hc.Coffee) []interface{} {
 	c := make(map[string]interface{})
 	c["id"] = coffee.ID
@@ -213,6 +176,5 @@ func flattenCoffee(coffee hc.Coffee) []interface{} {
 	c["description"] = coffee.Description
 	c["price"] = coffee.Price
 	c["image"] = coffee.Image
-
 	return []interface{}{c}
 }
